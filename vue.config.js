72ddb43c8d991fc6851b3899baa3f421e0cf5565
Ydog <2769053799@qq.com>
2023-04-11 13:46:33 +0800

    feat: 登录实现

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    https: false,
+    proxy: {
+      '/api': {
+        target: 'http://43.143.0.76:8889/api/private/v1/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 })
