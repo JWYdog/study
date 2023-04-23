9f5349c2ce2fb42c7a472fcfa99106800a8d17f4
JWYdog <129369045+JWYdog@users.noreply.github.com>
2023-04-23 17:10:35 +0800

    feat: i18n初使用

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import '@/router/permission'
-
+import i18n from './i18n'
 const app = createApp(App)
 app.use(ElementPlus)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-app.use(store).use(router)
+app.use(store).use(router).use(i18n)
 app.mount('#app')
