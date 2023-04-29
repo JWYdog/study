795717dc0a61e677e8caedeabbd0662868ebb185
JWYdog <129369045+JWYdog@users.noreply.github.com>
2023-04-29 19:20:17 +0800

    feat: 分页器

--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -8,7 +8,7 @@
           v-model="queryForm.query"
         ></el-input>
       </el-col>
-      <el-button type="primary" :icon="Search">{{
+      <el-button type="primary" :icon="Search" @click="initGetUsersList">{{
         $t('table.search')
       }}</el-button>
       <el-button type="primary">{{ $t('table.adduser') }}</el-button>
@@ -35,6 +35,18 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-pagination
+      v-model:current-page="queryForm.pagenum"
+      v-model:page-size="queryForm.pagesize"
+      :page-sizes="[2, 5, 10, 15]"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </el-card>
 </template>
 
@@ -49,14 +61,27 @@ const queryForm = ref({
   pagesize: 2
 })
 
+const total = ref(0)
+
 const tableData = ref([])
 
 const initGetUsersList = async () => {
   const res = await getUser(queryForm.value)
-  console.log(res)
+  total.value = res.total
   tableData.value = res.users
 }
 initGetUsersList()
+
+const handleSizeChange = (pageSize) => {
+  queryForm.value.pagenum = 1
+  queryForm.value.pagesize = pageSize
+  initGetUsersList()
+}
+
+const handleCurrentChange = (pagenum) => {
+  queryForm.value.pagenum = pagenum
+  initGetUsersList()
+}
 </script>
 
 <style lang="scss" scoped>
