03795be55589a8768f0f62264af5893348951085
JWYdog <129369045+JWYdog@users.noreply.github.com>
2023-04-29 13:59:31 +0800

    feat: 表格静态页

---
+++ src/views/users/options.js
@@ -0,0 +1,28 @@
+export const options = [
+  {
+    label: 'username',
+    prop: 'username'
+  },
+  {
+    label: 'email',
+    prop: 'email',
+    width: 200
+  },
+  {
+    label: 'mobile',
+    prop: 'mobile'
+  },
+  {
+    label: 'mg_state',
+    prop: 'mg_state'
+  },
+  {
+    label: 'create_time',
+    prop: 'create_time'
+  },
+  {
+    label: 'action',
+    prop: 'action',
+    width: 300
+  }
+]
